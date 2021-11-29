f11550090d4023752351b5a4884b66c303f27bf2
Ксения Финаева <polya.makova@outlook.com>
2021-11-29 11:34:41 +0300

    Исправлено

--- Task7.1/Program.cs
+++ Task7.1/Program.cs
@@ -13,34 +13,69 @@ namespace Task7._1
         */
         static void Main(string[] args)
         {
-            #region Инициализация переменных
             Console.WriteLine("Введите длины сторон 1-го треугольника: ");
-            int a1 = Convert.ToInt32(Console.ReadLine());
-            int b1 = Convert.ToInt32(Console.ReadLine());
-            int c1 = Convert.ToInt32(Console.ReadLine());
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            ExistTriangle(a1, b1, c1);
             Console.WriteLine("Введите длины сторон 2-го треугольника: ");
-            int a2 = Convert.ToInt32(Console.ReadLine());
-            int b2 = Convert.ToInt32(Console.ReadLine());
-            int c2 = Convert.ToInt32(Console.ReadLine());
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            double c2 = Convert.ToDouble(Console.ReadLine());
+            ExistTriangle(a2, b2, c2);
 
             double S1 = GetParam(a1, b1, c1); // запрос у метода вычисления площади для треугольника 1
             double S2 = GetParam(a2, b2, c2); // запрос у метода вычисления площади для треугольника 2
-            if (S1 > S2) // сравнение площадей двух треугольников
+
+            try
             {
-                Console.WriteLine("Площадь первого треугольника больше, чем площадь второго треугольника");
+            if (S1 == S2)
+            {
+                Console.WriteLine("Площади первого и второго треугольника равны");
+            }
+
+            else
+            {
+                if (S1 < S2)
+                {
+                    Console.WriteLine("Площадь второго треугольника больше, чем площадь первого треугольника");
+                }
+                else
+                {
+                    Console.WriteLine("Площадь первого треугольника больше, чем площадь второго треугольника");
+                }
+            }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool ExistTriangle(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                if (((a + b) >= c) && ((b + c) >= a) && ((a + c) >= b))
+                {
+                    Console.WriteLine("Треугольник существует");
+                }
+                return true;
             }
             else
             {
-                Console.WriteLine("Площадь второго треугольника больше, чем площадь первого треугольника");
+                Console.WriteLine("Треугольник не существует");
+                return false;
             }
-            #endregion
-            Console.ReadKey();
         }
-        static double GetParam(int a, int b, int c)
+        static double GetParam(double a, double b, double c)
         {
             double result = 0;
             double p = (a + b + c) / 2;
-            double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));  
+            result = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine();
             return result;
         }
     }
--- Task7.2/Program.cs
+++ Task7.2/Program.cs
@@ -11,12 +11,12 @@ namespace Task7._1
         // Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
         static void Main(string[] args)
         {
-            Console.Write("Введите длину ребра куба в метрах: ");
+            Console.Write("Введите длину ребра куба: ");
             int a = Convert.ToInt32(Console.ReadLine());
             double V, S;
             GetParam(a, out V, out S);
-            Console.WriteLine("Тогда объём куба равен {0} м3", V);
-            Console.WriteLine("А площадь куба равна {0} м2", S);
+            Console.WriteLine("Тогда объём куба равен {0}", V);
+            Console.WriteLine("А площадь куба равна {0}", S);
             Console.ReadKey();
         }
         static void GetParam(int a, out double V, out double S)
